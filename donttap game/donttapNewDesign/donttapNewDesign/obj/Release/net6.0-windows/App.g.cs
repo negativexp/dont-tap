75831efc0587533780ef25781bf73b75103b2db5
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-06-02 20:21:43 +0200

    watch me flex

--- donttap game/donttapNewDesign/donttapNewDesign/obj/Release/net6.0-windows/App.g.cs
+++ donttap game/donttapNewDesign/donttapNewDesign/obj/Release/net6.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace donttapNewDesign {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace donttapNewDesign {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             donttapNewDesign.App app = new donttapNewDesign.App();
             app.InitializeComponent();
