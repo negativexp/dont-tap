0d299191082056b3551138c91c9257669f0f7d62
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-06-09 13:00:44 +0200

    its not fucking working

--- donttap game/donttapNewDesign/donttapNewDesign/obj/Release/net6.0-windows/App.g.i.cs
+++ donttap game/donttapNewDesign/donttapNewDesign/obj/Release/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace donttapNewDesign {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -69,7 +69,7 @@ namespace donttapNewDesign {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
@@ -109,7 +109,7 @@ namespace donttapNewDesign {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public static void Main() {
             donttapNewDesign.App app = new donttapNewDesign.App();
             app.InitializeComponent();
--- donttap game/donttapNewDesign/donttapNewDesign/obj/Release/net6.0-windows/DONT TAP_Content.g.i.cs
+++ donttap game/donttapNewDesign/donttapNewDesign/obj/Release/net6.0-windows/DONT TAP_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bruh.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bruh.ico")]
 
 
