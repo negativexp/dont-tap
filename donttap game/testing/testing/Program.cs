3f4525d0029fc72b924b2d747f9a97f21a647ac4
evilbitmap <matyas.schuller@educhem.cz>
2022-06-10 10:22:32 +0200

    idk

---
+++ donttap game/testing/testing/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace testing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("  Enter city:");
+                Console.Write("->");
+                string x = Console.ReadLine();
+                var y = new[] { "x", "X" };
+                bool notSafe = false;
+                for(int i = 0; i < x.Length; i++)
+                {
+                    if(x[i] == 'x')
+                    {
+                        Console.WriteLine("Not Safe");
+                        notSafe = true;
+                        break;
+                    }
+                }
+                if (!notSafe)
+                    Console.WriteLine("Safe");
+                Console.ReadLine();
+            }
+        }
+    }
+}
